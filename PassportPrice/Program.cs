714cdabef42c9db0ad6e6e842dbdacbc170e13d5
Kolu <58555362+nissasin427@users.noreply.github.com>
2023-10-31 22:34:02 +0200

    Add files via upload

---
+++ PassportPrice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PassportPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            double minimalPrice = 0;
+            double startUpFee = 0.7;
+
+            if (n < 20)
+            {                
+                if (timeOfDay == "day")
+                {
+                    minimalPrice = startUpFee + n * 0.79;
+                }
+                else
+                {
+                    minimalPrice = startUpFee + n * 0.9;
+                }
+            }
+            else if (n > 100)
+            {
+                minimalPrice = n * 0.06;
+            }
+            else
+            {
+                minimalPrice = n * 0.09;
+            }
+
+            Console.WriteLine($"{minimalPrice:f2}");
+
+        }
+    }
+}
